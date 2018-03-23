644a26736b90a6493a2b76c5d9d2ba3f70b4d3e6
IQR Developer 2 <iqrmac2@IQRs-MacBook-Pro.local>
2018-03-23 19:33:08 +0800

    - add unsubscribe from push after logout

--- core/redux/sagas/saga.login.js
+++ core/redux/sagas/saga.login.js
@@ -49,10 +49,10 @@ function* loginFlow() {
       if (stage === STAGE_DEFINE.VERIFICATION_CODE) { // 输入验证码
         try {
           yield all([
-            put(account.loginPutValue(3)), 
+            put(account.loginPutValue(3)),
             put(application.showProgress())
           ])
-          
+
           let { id, code, isMail, phoneCountryCode, _id = '' } = value
           // 邮箱登录模式 - 二次验证码
           if (!_id) {
@@ -62,8 +62,8 @@ function* loginFlow() {
           //
 
           const path = isMail ? 'v1/auth/email' : 'v1/auth/phone'
-          const base = isMail ? 
-            { email: id, phoneVerificationCode: code, emailVerificationCode: code, _id } : 
+          const base = isMail ?
+            { email: id, phoneVerificationCode: code, emailVerificationCode: code, _id } :
             { phoneCountryCode, phoneNo: id, phoneVerificationCode: code, _id }
 
           const body = Object.assign({}, base, { latitude: 3.321, longitude: 1.23 })
@@ -71,7 +71,7 @@ function* loginFlow() {
             delete body._id
           }
           const { data }  = yield call(session.user.post, path, body)
-          
+
           yield call(loginSuccess, data) // 登录成功
 
         } catch (e) {
@@ -83,14 +83,14 @@ function* loginFlow() {
           )) {
             yield all([
               put(account.loginPutValue(2)),
-              put(application.hideProgress()), 
+              put(application.hideProgress()),
               put(application.showMessage(e.response.data.message)),
             ])
           } else if (e.response && e.response.data.code === 'MULTIPLE_USER_ACCOUNT') {
             const { id, code, phoneCountryCode } = value
             const path = isMail ? 'v1/auth/email' : 'v1/auth/phone'
-            const base = isMail ? 
-              { email: id, emailVerificationCode: code, } : 
+            const base = isMail ?
+              { email: id, emailVerificationCode: code, } :
               { phoneCountryCode, phoneNo: id, phoneVerificationCode: code, _id1: id }
             const body = Object.assign({}, base, { latitude: 3.321, longitude: 1.23 })
 
@@ -99,7 +99,7 @@ function* loginFlow() {
             yield put(NavigationActions.navigate({ routeName: 'LoginSelectAccount', params: { data: e.response.data.data, value } }))
             yield put(account.loginPutValue(2))
           } else if (e.response && (
-            e.response.data.code == 'INVALID_USER' || 
+            e.response.data.code == 'INVALID_USER' ||
             e.response.data.code == 'INCOMPLETE_REGISTRATION'
           )) { // 跳转注册
             const actions = [
@@ -113,13 +113,13 @@ function* loginFlow() {
             const url = 'v1/sendVerificationCode/email'
             yield call(session.user.post, url, { email: id })
             yield all([
-              put(application.hideProgress()), 
+              put(application.hideProgress()),
               put(application.showMessage('已将验证码发送至您的手机')),
               put(account.loginPutValue(2))
             ])
           } else {
             const flow = [
-              put(application.hideProgress()), 
+              put(application.hideProgress()),
               put(application.showMessage('无法连接到服务器，请稍后再试')),
             ]
             if (isMail) {
@@ -136,12 +136,12 @@ function* loginFlow() {
       if (stage === STAGE_DEFINE.READY_REGISTER) {
         try {
           yield all([
-            put(account.loginPutValue(3)), 
+            put(account.loginPutValue(3)),
             put(application.showProgress())
           ])
 
-          const register = yield call(session.User.Post, 'v1/register', Object.assign({}, 
-            value, 
+          const register = yield call(session.User.Post, 'v1/register', Object.assign({},
+            value,
             { latitude: 3.321, longitude: 1.23 }
           ))
 
@@ -151,19 +151,19 @@ function* loginFlow() {
         } catch (e) {
           if (e.response && e.response.data.message === 'INVALID_REFERRAL') {
             yield all([
-              put(application.hideProgress()), 
+              put(application.hideProgress()),
               put(application.showMessage(e.response.data.message)),
             ])
           } else if (e.response && e.response.data.message) {
             yield all([
               put(account.loginPutValue(2)),
-              put(application.hideProgress()), 
+              put(application.hideProgress()),
               put(application.showMessage(e.response.data.message)),
             ])
           } else {
             yield all([
               put(account.loginPutValue(2)),
-              put(application.hideProgress()), 
+              put(application.hideProgress()),
               put(application.showMessage('无法连接到服务器，请稍后再试')),
             ])
           }
@@ -181,6 +181,7 @@ function* logoutFlow() {
     const payload = yield take(account.logoutSuccess().type)
     yield put(booking.passengerSetValue({ type: 'circle', name: '优选', payment: '现金支付', book: false, time: 'now', selected_friends: [] }))
     yield put(account.loginPutValue(0))
+    yield call(session.Push.Put, `v1/unsubscribe`, { uuid: System.UUID })
     yield put(NavigationActions.navigate({ routeName: 'AuthLoading' }))
   }
 }
@@ -188,7 +189,7 @@ function* logoutFlow() {
 function* registerDevice() {
   while(true) {
     yield take(application.updatePushToken().type)
-    const { token, userId, user_id, status } = yield select(state => ({ 
+    const { token, userId, user_id, status } = yield select(state => ({
       token: state.storage.push_service_token,
       userId: state.storage.baidu_user_id,
       user_id: state.account.user._id,
@@ -200,23 +201,23 @@ function* registerDevice() {
       const extendFields = System.Platform.Android ? { channelId: token, userId } : { token }
       const postData = Object.assign({}, {
         'device' : {
-          'available' : true, 
-          'platform' : System.Platform.Android ? 'Android' : 'iOS', 
-          'version' : System.Device.Version, 
-          'uuid' : System.UUID, 
-          'cordova' : '0.0.0', 
-          'model' : System.Device.Name, 
-          'manufacturer' : System.Device.Brand, 
-          'isVirtual' : false, 
+          'available' : true,
+          'platform' : System.Platform.Android ? 'Android' : 'iOS',
+          'version' : System.Device.Version,
+          'uuid' : System.UUID,
+          'cordova' : '0.0.0',
+          'model' : System.Device.Name,
+          'manufacturer' : System.Device.Brand,
+          'isVirtual' : false,
           'serial' : 'UNKNOW'
-        }, 
+        },
         'status' : 'active',
-        'type' : System.Platform.Name, 
+        'type' : System.Platform.Name,
         'uuid' : System.UUID,
         'user_id': user_id
       }, extendFields)
-    
-      yield call(session.Push.Post, 'v1/register', postData) 
+
+      yield call(session.Push.Post, 'v1/register', postData)
     } catch (e) {
       // console.info('[推送][注册设备][失败]')
     }
