ed47614398a7318ba2826cebc2b7a4a645e4c2e1
Mollet <mollet@dacsee.com>
2018-03-28 18:37:26 +0800

    replace the api

--- core/route/main/passenger/index.js
+++ core/route/main/passenger/index.js
@@ -35,9 +35,6 @@ const PIN_HEIGHT = ((height - 22) / 2)
 export default connect(state => ({ ...state.booking }))(class PassengerComponent extends Component {
 
   constructor(props) {
-
-    console.log(props)
-
     super(props)
     this.state = {
       drag: false,
@@ -160,7 +157,7 @@ export default connect(state => ({ ...state.booking }))(class PassengerComponent
       Animated.timing(this.pin, { toValue: 0, duration: 200 }).start()
       Animated.timing(this.board, { toValue: 0, duration: 200 }).start()
 
-      const data = await Session.Lookup_CN.Get(`v1/map/search/geo/${latitude},${longitude}`)
+      const { data } = await Session.Lookup_CN.Get(`v1/map/search/geo/${latitude},${longitude}`)
       this.props.dispatch(booking.passengerSetValue({ from: data || {} }))
       this.setState({ drag: false })
     } catch (e) {
