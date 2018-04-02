c77caa1adbd10d7edb3a9ab8a8384e1e65548b12
Mollet <mollet@dacsee.com>
2018-04-02 16:35:08 +0800

    font  adaptation

--- core/utils/util.textSize.js
+++ core/utils/util.textSize.js
@@ -11,17 +11,17 @@ export default {
     if (dpr === 2) {
       // iphone 5s和老安卓机
       if (width < 360) {
-        return size * 0.95;
+        return size * 0.90;
       }
       // iphone 5
       if (height < 667) {
         return size;
         // iphone 6-6s
       } else if (height >= 667 && height <= 735) {
-        return size * 1.15;
+        return size * 1.1;
       }
       // older phablets
-      return size * 1.25;
+      return size * 1.20;
     }
     if (dpr === 3) {
       // catch Android font scaling on small machines
@@ -31,16 +31,16 @@ export default {
       }
       // Catch other weird android width sizings
       if (height < 667) {
-        return size * 1.15;
+        return size * 1.1;
         // catch in-between size Androids and scale font up
         // a tad but not too much
       }
       if (height >= 667 && height <= 735) {
-        return size * 1.2;
+        return size * 1.15;
       }
       // catch larger devices
       // ie iphone 6s plus / 7 plus / mi note 等等
-      return size * 1.27;
+      return size * 1.22;
     }
     if (dpr === 3.5) {
       // catch Android font scaling on small machines
@@ -50,15 +50,15 @@ export default {
         // Catch other smaller android height sizings
       }
       if (height < 667) {
-        return size * 1.20;
+        return size * 1.15;
         // catch in-between size Androids and scale font up
         // a tad but not too much
       }
       if (height >= 667 && height <= 735) {
-        return size * 1.25;
+        return size * 1.20;
       }
       // catch larger phablet devices
-      return size * 1.40;
+      return size * 1.30;
     }
     // if older device ie pixelRatio !== 2 || 3 || 3.5
     return size;
